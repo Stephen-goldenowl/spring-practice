0724f538977f2cae3f2ef5532af082dc511f7ea2
giabao0301 <21521866@gm.uit.edu.vn>
2025-04-04 14:07:15 +0700

    changed allowed origins (security unabled)

--- src/main/java/com/gscores/backend/config/CorsConfig.java
+++ src/main/java/com/gscores/backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://g-scores-frontend.vercel.app")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
